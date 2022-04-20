37c2e9e63c9e6ca145dfcb771e4954e474a73b88
Gleb <gleb.kvasov26@mail.ru>
2022-04-20 13:10:10 +0300

    Добавьте файлы проекта.

---
+++ dz 5/dz 5.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+using namespace std;
+
+void change(int* arr, int size) {
+	int a;
+	if (size % 2 == 0) {
+		for (int i = 0; i < size; i += 2) {
+			a = arr[i];
+			arr[i] = arr[i + 1];
+			arr[i + 1] = a;
+		}
+	}
+	if (size % 2 != 0) {
+		for (int i = 0; i < size-1; i += 2) {
+			a = arr[i];
+			arr[i] = arr[i + 1];
+			arr[i + 1] = a;
+		}
+	}
+}
+
+int main()
+{
+	int const size = 13;
+	int* arr = new int[size];
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand()%100 - 50;
+	}
+	change(arr, size);
+	for (int i = 0; i < size; i++) {
+		cout <<arr[i]<<" ";
+	}
+	delete[] arr;
+	
+}
